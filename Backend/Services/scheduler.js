87eac6cae2ff15882b48c0aae74d1b756fc30623
Ivan <janivantot@gmail.com>
2024-12-06 12:13:55 +0800

    added scheduler.js

---
+++ Backend/Services/scheduler.js
@@ -0,0 +1,7 @@
+import schedule from 'node-schedule';
+
+const job = schedule.scheduleJob('13 12 * * *', function () {
+    console.log('Hello, World!');
+});
+
+console.log('Job scheduled to print "Hello, World!" at 12:13 PM.');
