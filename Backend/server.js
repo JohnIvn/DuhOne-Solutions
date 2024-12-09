082bab5402ab4d81f6b5a96a19ed990ef18b9e94
Ivan <janivantot@gmail.com>
2024-12-09 23:38:04 +0800

    fixed cors/socket bug

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
-import { Server } from "socket.io"; 
+import { Server } from "socket.io";
 
- const configureSockets = (server) => {
+const configureSockets = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173", 
+      origin: ["http://localhost:5173", "http://localhost:5174"], 
       methods: ["GET", "POST"],
     },
   });
@@ -15,7 +15,6 @@ import { Server } from "socket.io";
       console.log(`User registered with ID: ${userId}`);
     });
 
-
     socket.on("adminMessage", ({ userId, message }) => {
       console.log(`Admin sent message to User ${userId}: ${message}`);
       io.to(userId).emit("message", { message });
@@ -24,7 +23,6 @@ import { Server } from "socket.io";
     socket.on("disconnect", (reason) => {
       console.log(`Socket disconnected: ${socket.id} due to ${reason}`);
     });
-
   });
 };
 
